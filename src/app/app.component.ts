e734c3ea03ee0383fdc4c8428c212084b4b1f327
Dmytro <dimasik22101993@gmail.com>
2022-09-18 13:55:55 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'smtp-test-front';
+  form!: FormGroup;
+  output: any;
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      fromName: new FormControl('', [Validators.required]),
+      fromEmail: new FormControl('', [
+        Validators.required, Validators.email
+      ]),
+      to: new FormControl('', [
+        Validators.required, Validators.email
+      ]),
+      subject: new FormControl('', [Validators.required]),
+      text: new FormControl('', []),
+      html: new FormControl('', []),
+      smtpHost: new FormControl('', [Validators.required]),
+      smtpPort: new FormControl('', [Validators.required, Validators.min(1)]),
+      smtpSecure: new FormControl('', []),
+      smtpAuthUser: new FormControl('', [
+        Validators.required, Validators.email
+      ]),
+      smtpAuthPass: new FormControl('', [Validators.required]),
+    });
+  }
+
+  send() {
+    const {value} = this.form;
+    value.smtpPort = +value.smtpPort;
+    value.smtpSecure = !!value.smtpSecure;
+
+    this.http.post('http://localhost:3000', value)
+      .subscribe(
+        res => this.output = JSON.stringify(res, null, 2),
+        err => this.output = JSON.stringify(err, null, 2)
+      );
+  }
 }
